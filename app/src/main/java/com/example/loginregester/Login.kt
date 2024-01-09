e933619f603e2dbb52717cd68cb182baa5329c25
MehdiMarjani <113903979+Sevilex@users.noreply.github.com>
2024-01-09 07:46:22 -0800

    Create Login Page

--- app/src/main/java/com/example/loginregester/Login.kt
+++ app/src/main/java/com/example/loginregester/Login.kt
@@ -15,8 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
@@ -48,59 +47,56 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 @Preview(showBackground = true)
 @Composable
-fun RegisterForm() {
+fun LoginForm() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-
         Image(
-            painter = painterResource(id = R.drawable.register_background),
+            painter = painterResource(id = R.drawable.login_background),
             contentDescription = "",
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
         )
 
         Column {
             Box(
-                modifier = Modifier.padding(
+                Modifier.padding(
+                    start = 230.dp,
                     top = 300.dp,
-                    start = 200.dp,
                     bottom = 100.dp
                 )
             ) {
                 Text(
-                    text = "Register",
-                    //fontSize = 48.dp,
-                    modifier = Modifier.padding(1.dp),
+                    text = "Login",
+                    fontSize = 48.sp,
                     fontWeight = FontWeight.Bold,
                     color = Color(0xFF666666),
                 )
             }
             Column {
-                UserNameField()
-                EmailFiled()
-                PasswordField()
-                CheckField()
-                ButtonField()
-
+                UserNameFieldLogin()
+                PasswordFieldLogin()
+                ArrowForwardFieldLogin()
+                ForgetFieldLogin()
+                RegisterFieldLogin()
             }
         }
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun UserNameField() {
+fun UserNameFieldLogin() {
     var txt by remember { mutableStateOf("") }
     TextField(
         value = txt,
         onValueChange = { txt = it },
         label = {
             Text(
-                text = "UserName",
+                text = "Username",
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFFAAAAAA)
             )
@@ -115,7 +111,7 @@ private fun UserNameField() {
             Icon(
                 imageVector = Icons.Filled.Person,
                 contentDescription = "",
-                tint = Color(0xFFFAFAFA)
+                tint = Color(0xFFCCCCCC)
             )
         },
         modifier = Modifier
@@ -128,46 +124,10 @@ private fun UserNameField() {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun EmailFiled() {
-    var txt2 by remember { mutableStateOf("") }
-    TextField(
-        value = txt2,
-        onValueChange = { txt2 = it },
-        label = {
-            Text(
-                text = "Email",
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFFAAAAAA)
-            )
-        },
-        //shape = RoundedCornerShape(topEnd = 48.dp),
-        colors = TextFieldDefaults.textFieldColors(
-            unfocusedIndicatorColor = Color(0xFFDDDDDD),
-            focusedIndicatorColor = Color(0xFFCCCCCC),
-            //backgroundColor = Color(0xFFFAFAFA)
-        ),
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Filled.Email,
-                contentDescription = "",
-                tint = Color(0xFFCCCCCC)
-            )
-        },
-        modifier = Modifier
-            .width(400.dp)
-            .border(
-                width = 1.dp,
-                color = Color(0xFFDDDDDD),
-                //shape = RoundedCornerShape(topEnd = 48.dp),
-            )
-    )
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun PasswordField() {
+fun PasswordFieldLogin() {
     var password by remember { mutableStateOf("") }
     TextField(
         value = password,
@@ -184,6 +144,11 @@ private fun PasswordField() {
             keyboardType = KeyboardType.Password
         ),
         visualTransformation = PasswordVisualTransformation(),
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            //backgroundColor = Color(0xFFFAFAFA)
+        ),
         leadingIcon = {
             Icon(
                 imageVector = Icons.Filled.Lock,
@@ -196,18 +161,13 @@ private fun PasswordField() {
             .shadow(
                 elevation = 10.dp,
                 shape = RoundedCornerShape(bottomEnd = 48.dp)
-            ),
-        colors = TextFieldDefaults.textFieldColors(
-            unfocusedIndicatorColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            //backgroundColor = Color(0xFFFAFAFA)
-        )
+            )
     )
 }
 
 @Composable
-private fun CheckField(){
-    Box(Modifier.absoluteOffset(370.dp, (-120).dp)) {
+fun ArrowForwardFieldLogin() {
+    Box(Modifier.absoluteOffset(370.dp, (-90).dp)) {
         IconButton(
             onClick = {},
             modifier = Modifier
@@ -223,7 +183,7 @@ private fun CheckField(){
                 )
         ) {
             Icon(
-                imageVector = Icons.Filled.Check,
+                imageVector = Icons.Filled.ArrowForward,
                 contentDescription = "",
                 tint = Color.White,
                 modifier = Modifier.size(32.dp)
@@ -233,13 +193,28 @@ private fun CheckField(){
 }
 
 @Composable
-private fun ButtonField(){
-    Box(Modifier.fillMaxWidth(), Alignment.TopEnd) {
+fun ForgetFieldLogin() {
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp), Alignment.TopEnd
+    ) {
+        Text(
+            text = "Forgot ?",
+            fontSize = 16.sp,
+            color = Color(0xFF888888)
+        )
+    }
+}
+
+@Composable
+fun RegisterFieldLogin() {
+    Box(Modifier.fillMaxWidth(), Alignment.TopStart) {
         Button(
             onClick = {},
             shape = RoundedCornerShape(
-                topStart= 48.dp,
-                bottomStart = 48.dp
+                topEnd = 48.dp,
+                bottomEnd = 48.dp
             ),
             colors = ButtonDefaults.buttonColors(
                 //backgroundColor = Color(0xFFFAFAFA),
@@ -249,21 +224,21 @@ private fun ButtonField(){
                 .shadow(
                     elevation = 5.dp,
                     shape = RoundedCornerShape(
-                        topStart = 48.dp,
-                        bottomStart = 48.dp
+                        topEnd = 48.dp,
+                        bottomEnd = 48.dp
                     )
                 )
                 .border(
                     width = 1.dp,
                     color = Color(0xFFDDDDDD),
                     shape = RoundedCornerShape(
-                        topStart = 48.dp,
-                        bottomStart = 48.dp
+                        topEnd = 48.dp,
+                        bottomEnd = 48.dp
                     )
                 )
         ) {
             Text(
-                text = "Login",
+                text = "Register",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(12.dp)
