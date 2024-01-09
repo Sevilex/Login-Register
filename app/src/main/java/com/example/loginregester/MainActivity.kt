e88af0229ae307f0417c250f60496347640f9b28
MehdiMarjani <113903979+Sevilex@users.noreply.github.com>
2024-01-09 04:54:10 -0800

    Create Register From

--- app/src/main/java/com/example/loginregester/MainActivity.kt
+++ app/src/main/java/com/example/loginregester/MainActivity.kt
@@ -17,13 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LoginRegesterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    RegisterForm()
-                }
+                RegisterForm()
             }
         }
     }
@@ -31,6 +25,11 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun RegisterForm(){
+fun RegisterForm() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
 
+    }
 }
